402295b2974a403dab226d0ac665a5c551d92eb0
Srivarshan <srivarshan02@gmail.com>
2021-11-23 11:03:08 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import numpy
+import sys
 from model import model
-from data import chars, dataX
+from data import chars, dataX, n_vocab
 
 
 print("Enter path to saved weights:")
@@ -20,3 +21,16 @@ start = numpy.random.randint(0, len(dataX)-1)
 pattern = dataX[start]
 print("Seed:")
 print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
+
+# create the report
+for i in range(1000):
+	x = numpy.reshape(pattern, (1, len(pattern), 1))
+	x = x / float(n_vocab)
+	prediction = model.predict(x, verbose=0)
+	index = numpy.argmax(prediction)
+	result = int_to_char[index]
+	seq_in = [int_to_char[value] for value in pattern]
+	sys.stdout.write(result)
+	pattern.append(index)
+	pattern = pattern[1:len(pattern)]
+print("\nDone.")
