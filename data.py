199098dbba4ec6484566c88a0dc95bfd19171897
Srivarshan <srivarshan02@gmail.com>
2021-11-23 10:22:40 +0530

    Initial commit

---
+++ data.py
@@ -0,0 +1,36 @@
+print("Enter path to data file:")
+filename = input()
+
+# load ascii text and covert to lowercase
+raw_text = open(filename, 'r', encoding='utf-8').read()
+raw_text = raw_text.lower()
+
+# create mapping of unique chars to integers
+chars = sorted(list(set(raw_text)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+
+n_chars = len(raw_text)
+n_vocab = len(chars)
+print("Total Characters: ", n_chars)
+print("Total Vocab: ", n_vocab)
+
+# prepare the dataset of input to output pairs encoded as integers
+seq_length = 100
+dataX = []
+dataY = []
+for i in range(0, n_chars - seq_length, 1):
+	seq_in = raw_text[i:i + seq_length]
+	seq_out = raw_text[i + seq_length]
+	dataX.append([char_to_int[char] for char in seq_in])
+	dataY.append(char_to_int[seq_out])
+n_patterns = len(dataX)
+print("Total Patterns: ", n_patterns)
+
+# reshape X to be [samples, time steps, features]
+X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+
+# normalize
+X = X / float(n_vocab)
+
+# one hot encode the output variable
+y = to_categorical(dataY)
